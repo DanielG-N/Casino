e2fe5e1ebf35685eb3dee95ca84631bfd1b64da5
satanscorner <adeniro@student.neumont.edu>
2022-02-17 16:04:38 -0700

    menu rough draft finished

---
+++ Menu/menu.js
@@ -0,0 +1,7 @@
+function StartGame() {
+    location.href = 'http://127.0.0.1:5500/Menu/menu.html';
+}
+
+function BackButton() {
+    location.href = 'http://127.0.0.1:5500/Menu/startMenu.html';
+}
